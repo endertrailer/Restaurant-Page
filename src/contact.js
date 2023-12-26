fa1969d1b01c5955f123c4e089e3d08a478e96f0
endertrailer <anismasoom80@gmail.com>
2023-12-26 22:17:29 +0530

    finish restraunt page

--- src/contact.js
+++ src/contact.js
@@ -1,32 +1,117 @@
 export { contactPage };
 function contactPage(){
   const content = document.querySelector('#content');
-  const headbar = document.createElement('div');
-  const selection = document.createElement('div');
-  const selectionHome = document.createElement('div');
-  const selectionMenu = document.createElement('div');
-  const selectionContact = document.createElement('div');
-  
-  
-  
-  
-  headbar.classList.add('headbar');
-  selection.classList.add('selections');
-  selectionHome.classList.add('selection');
-  selectionMenu.classList.add('selection');
-  selectionContact.style.backgroundColor = 'cornflowerblue';
-  selectionContact.classList.add('selection');
-  selectionHome.textContent = 'Home';
-  selectionMenu.textContent = 'Menu';
-  selectionContact.textContent = 'Contact';
-  
-  
-  
-  selection.appendChild(selectionHome);
-  selection.appendChild(selectionMenu);
-  selection.appendChild(selectionContact);
-  headbar.appendChild(selection);
-  content.appendChild(headbar);
+ // Create elements and set their classes, text content, and attributes
+const headbar = document.createElement('div');
+headbar.classList.add('headbar');
+
+const selections = document.createElement('div');
+selections.classList.add('selections');
+
+const selectionHome = document.createElement('div');
+selectionHome.classList.add('selection');
+selectionHome.textContent = 'Home';
+
+const selectionMenu = document.createElement('div');
+selectionMenu.classList.add('selection');
+selectionMenu.textContent = 'Menu';
+
+const selectionContact = document.createElement('div');
+selectionContact.classList.add('selection');
+selectionContact.textContent = 'Contact';
+selectionContact.style.backgroundColor = 'cornflowerblue';
+
+const midarea = document.createElement('div');
+midarea.classList.add('midarea');
+
+const barbox = document.createElement('div');
+barbox.classList.add('barbox');
+
+const bar = document.createElement('div');
+bar.classList.add('bar');
+bar.textContent = 'Contact us';
+
+const contacts = document.createElement('div');
+contacts.classList.add('contacts');
+
+// Details of contacts
+const contactDetails = [
+    {
+        name: 'Manager Rob Banks',
+        email: 'RobBanks1100998@Astro.com',
+        phone: '111-999-222',
+        address: 'GalactiCom: ST-ALPH-TH9',
+        image: '../src/images/manager.jpeg'
+    },
+    {
+        name: 'Reciption Rick Sanchez',
+        email: 'Ricksanchez57811@Astro.com',
+        phone: '555-222-777',
+        address: 'GalactiCom: NB-NX-3G6T1D',
+        image: '../src/images/rick.jpeg'
+    },
+    {
+        name: 'Chef Luna Ling',
+        email: 'Lunaling55466288@Astro.com',
+        phone: '111-999-222',
+        address: 'GalactiCom: ST-ALPH-TH9',
+        image: '../src/images/chef.jpeg'
+    }
+];
+
+// Create contact boxes using contact details
+contactDetails.forEach(contact => {
+    const contactbox = document.createElement('div');
+    contactbox.classList.add('contactbox');
+
+    const contact_text = document.createElement('div');
+    contact_text.classList.add('contact_text');
+
+    const contact_name = document.createElement('div');
+    contact_name.classList.add('contact_name');
+    contact_name.textContent = contact.name;
+
+    const contact_id = document.createElement('div');
+    contact_id.classList.add('contact_id');
+
+    const email = document.createElement('div');
+    email.classList.add('email');
+    email.textContent = contact.email;
+
+    const phone = document.createElement('div');
+    phone.classList.add('phone');
+    phone.textContent = contact.phone;
+
+    const contact_address = document.createElement('div');
+    contact_address.classList.add('contact_address');
+    contact_address.textContent = contact.address;
+
+    const contact_img = document.createElement('img');
+    contact_img.classList.add('contact_img');
+    contact_img.alt = 'some';
+    contact_img.src = contact.image;
+
+    contact_id.appendChild(email);
+    contact_id.appendChild(phone);
+    contact_text.appendChild(contact_name);
+    contact_text.appendChild(contact_id);
+    contact_text.appendChild(contact_address);
+    contactbox.appendChild(contact_text);
+    contactbox.appendChild(contact_img);
+    contacts.appendChild(contactbox);
+});
+
+// Append the created elements to their respective parent elements
+selections.appendChild(selectionHome);
+selections.appendChild(selectionMenu);
+selections.appendChild(selectionContact);
+headbar.appendChild(selections);
+content.appendChild(headbar);
+barbox.appendChild(bar);
+midarea.appendChild(barbox);
+midarea.appendChild(contacts);
+content.appendChild(midarea);
+
   
   
 
--- src/home.js
+++ src/home.js
@@ -1,70 +1,86 @@
-export { loadPage };
-function loadPage(){
-const content = document.querySelector('#content');
-const headbar = document.createElement('div');
-const selection = document.createElement('div');
-const selectionHome = document.createElement('div');
-const selectionMenu = document.createElement('div');
-const selectionContact = document.createElement('div');
-const midArea = document.createElement('div');
-const barBox = document.createElement('div');
-const bar = document.createElement('div');
-const mid_section = document.createElement('div');
-
-
-
-
-headbar.classList.add('headbar');
-selection.classList.add('selections');
-selectionHome.classList.add('selection');
-selectionHome.style.backgroundColor = 'cornflowerblue';
-selectionMenu.classList.add('selection');
-selectionContact.classList.add('selection');
-selectionHome.textContent = 'Home';
-selectionMenu.textContent = 'Menu';
-selectionContact.textContent = 'Contact';
-midArea.classList.add('midarea');
-barBox.classList.add('barbox');
-bar.classList.add('bar');
-bar.textContent = 'Welcome to AstroBite restrauant';
-mid_section.classList.add('mid-section');
-
-
-selection.appendChild(selectionHome);
-selection.appendChild(selectionMenu);
-selection.appendChild(selectionContact);
-headbar.appendChild(selection);
-content.appendChild(headbar);
-content.appendChild(midArea);
-midArea.appendChild(barBox);
-barBox.appendChild(bar);
-midArea.appendChild(mid_section);
-
-const texts = [
-    'Indulge in the extraordinary at AstroBite, where we bring the universe to your plate. Taste the cosmos with celestial cuisine that is truly out of this world. Join us for a stellar dining experience that is simply cosmic!',
-    ` 
-    Celestial Day: 8:00 AM - 8:00 PM
-    Astronomy Day: 6:00 AM - 6:00 PM
-    Cosmic Day: 6:00 AM - 6:00 PM
-    Nebula Day: 6:00 AM - 6:00 PM
-    Stardust Day: 6:00 AM - 10:00 PM
-    Galaxy Day: 6:00 AM - 10:00 PM
-    Starlight Day: 8:00 AM - 10:00 PM
-    `,
-    'Restaurant Location: Orion\'s Nebula, Deep Space Sector 7'
-  ];
-
-  for (const text of texts) {
-    const midBox = document.createElement('div');
-    midBox.className = 'mid-box';
-    
-    const midText = document.createElement('div');
-    midText.className = 'mid-text';
-    midText.textContent = text;
-    
-    midBox.appendChild(midText);
-    mid_section.appendChild(midBox);
-  }
+export function loadPage() {
+  const content = document.querySelector('#content');
   
-
+  // Creating elements for header, selections, and midarea
+  const headbar = document.createElement('div');
+  headbar.classList.add('headbar');
+  
+  const selections = document.createElement('div');
+  selections.classList.add('selections');
+  
+  const home = document.createElement('div');
+  home.classList.add('selection');
+  home.textContent = 'Home';
+  home.style.backgroundColor = 'cornflowerblue';
+  
+  const menu = document.createElement('div');
+  menu.classList.add('selection');
+  menu.textContent = 'Menu';
+  
+  const contact = document.createElement('div');
+  contact.classList.add('selection');
+  contact.textContent = 'Contact';
+  
+  const midArea = document.createElement('div');
+  midArea.classList.add('midarea');
+  
+  // Appending selections to header and header to content
+  selections.appendChild(home);
+  selections.appendChild(menu);
+  selections.appendChild(contact);
+  headbar.appendChild(selections);
+  content.appendChild(headbar);
+  
+  // Creating elements for barbox, bar, mid-section, and description
+  const barBox = document.createElement('div');
+  barBox.classList.add('barbox');
+  
+  const bar = document.createElement('div');
+  bar.classList.add('bar');
+  bar.textContent = 'Welcome to AstroBite restaurant';
+  
+  const midSection = document.createElement('div');
+  midSection.classList.add('mid-section');
+  
+  const description = document.createElement('div');
+  description.classList.add('mid-desc');
+  description.innerHTML = `
+    <img src="https://th.bing.com/th/id/OIG.ZVIEi.aopPbet7YzuPG_?pid=ImgGn" alt="restaurant" class="view">
+    <div class="description">
+      Indulge in the extraordinary at AstroBite, where we bring the universe to your plate. Taste the cosmos with celestial cuisine that is truly out of this world. Join us for a stellar dining experience that is simply cosmic!
+    </div>
+  `;
+  
+  const timings = document.createElement('div');
+  timings.classList.add('mid-box');
+  timings.innerHTML = `
+    <div class="mid-text">
+      Timings:
+      <br>
+      Celestial Day: 8:00 AM - 8:00 PM<br>
+      Astronomy Day: 6:00 AM - 6:00 PM<br>
+      Cosmic Day: 6:00 AM - 6:00 PM<br>
+      Nebula Day: 6:00 AM - 6:00 PM<br>
+      Stardust Day: 6:00 AM - 10:00 PM<br>
+      Galaxy Day: 6:00 AM - 10:00 PM<br>
+      Starlight Day: 8:00 AM - 10:00 PM
+    </div>
+  `;
+  
+  const location = document.createElement('div');
+  location.classList.add('mid-box');
+  location.innerHTML = `
+    <div class="mid-text">Restaurant Location: Orion's Nebula, Deep Space Sector 7</div>
+  `;
+  
+  // Appending elements to their respective parents
+  barBox.appendChild(bar);
+  midSection.appendChild(description);
+  midSection.appendChild(timings);
+  midSection.appendChild(location);
+  
+  midArea.appendChild(barBox);
+  midArea.appendChild(midSection);
+  
+  content.appendChild(midArea);
 }
